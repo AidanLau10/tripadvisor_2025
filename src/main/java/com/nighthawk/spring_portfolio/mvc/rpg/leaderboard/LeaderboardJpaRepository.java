4e4986f18fa77dcdd144a6e3c992685870ac82ea
tanav-kambhampati <fifteentanav@gmail.com>
2024-10-21 08:53:13 -0700

    leaderboard

--- src/main/java/com/nighthawk/spring_portfolio/mvc/rpg/leaderboard/LeaderboardJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/rpg/leaderboard/LeaderboardJpaRepository.java
@@ -1,5 +1,17 @@
 package com.nighthawk.spring_portfolio.mvc.rpg.leaderboard;
 
-public class LeaderboardJpaRepository {
-    
+import java.util.List;
+
+import jakarta.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nighthawk.spring_portfolio.mvc.rpg.player.Player;
+
+public interface LeaderboardJpaRepository extends JpaRepository<Leaderboard, Long> {
+    List<Player> findByPlayerId(Long id);
+
+    @Transactional
+    void deleteByPlayerId(long id);
 }
+
