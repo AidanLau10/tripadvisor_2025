87b3d47751bc45a9ffa539430d1a268d305e06e0
tanav-kambhampati <fifteentanav@gmail.com>
2024-10-21 08:53:13 -0700

    leaderboard

--- src/main/java/com/nighthawk/spring_portfolio/mvc/rpg/leaderboard/Leaderboard.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/rpg/leaderboard/Leaderboard.java
@@ -1,5 +1,52 @@
 package com.nighthawk.spring_portfolio.mvc.rpg.leaderboard;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.nighthawk.spring_portfolio.mvc.rpg.player.Player;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Leaderboard {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Column(columnDefinition="RANK")
+    private long rank;
+
+    @NotNull
+    @Column(columnDefinition="NAME")
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "player_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Player player;
+
+    public Leaderboard(String name, Player player) {
+        this.name = name;
+        this.player = player;
+    }
+
+
 }
