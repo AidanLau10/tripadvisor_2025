2b73b263969395f27a59316c7102444d737d98ad
AidanLau10 <aidanlau10@gmail.com>
2024-10-27 00:13:22 -0700

    fixed question and player objects in submitting answers

--- src/main/java/com/nighthawk/spring_portfolio/mvc/rpg/answer/AnswerApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/rpg/answer/AnswerApiController.java
@@ -1,5 +1,7 @@
 package com.nighthawk.spring_portfolio.mvc.rpg.answer;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,10 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nighthawk.spring_portfolio.mvc.rpg.player.Player;
+import com.nighthawk.spring_portfolio.mvc.rpg.player.PlayerJpaRepository;
 import com.nighthawk.spring_portfolio.mvc.rpg.question.Question;
+import com.nighthawk.spring_portfolio.mvc.rpg.question.QuestionJpaRepository;
 
 import lombok.Getter;
-
 
 
 @RestController
@@ -22,17 +25,32 @@ public class AnswerApiController {
     @Autowired
     private AnswerJpaRepository answerJpaRepository;
 
+    @Autowired
+    private QuestionJpaRepository questionJpaRepository;
+
+    @Autowired
+    private PlayerJpaRepository playerJpaRepository;
     @Getter 
     public static class AnswerDto {
         private String content;
-        private Question question;
-        private Player player;
+        private Long questionId;
+        private Long playerId;
         private Long chatScore; 
     }
+
+
     @PostMapping("/submitanswer") 
     public ResponseEntity<Answer> postAnswer(@RequestBody AnswerDto answerDto) {
-        Answer answer = new Answer(answerDto.getContent(), answerDto.getQuestion(), answerDto.getPlayer(), answerDto.getChatScore());
+        Optional<Question> questionOpt = questionJpaRepository.findById(answerDto.getQuestionId());
+        Optional<Player> playerOpt = playerJpaRepository.findById(answerDto.getPlayerId());
+
+        
+        Question question = questionOpt.get();
+        Player player = playerOpt.get();
+
+        Answer answer = new Answer(answerDto.getContent(), question, player, answerDto.getChatScore());
         answerJpaRepository.save(answer);
+        
         
         return new ResponseEntity<>(answer, HttpStatus.OK);
     }
